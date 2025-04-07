85905a4746c928a291b548826134330eabb9c88f
min <aqwe78965@gmail.com>
2025-04-07 23:37:26 +0800

    feat: draw snake

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JPanel {
+
+    public static final int CELL_SIZE = 20;
+    public static int width = 400;
+    public static int height = 400;
+    public static int row = height / CELL_SIZE;
+    public static int col = width / CELL_SIZE;
+    private Snake snake;
+
+    public Main() {
+        snake = new Snake();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        g.fillRect(0, 0, width, height);
+        snake.drawSnake(g);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(width, height);
+    }
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Snack game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setContentPane(new Main());
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        window.setResizable(false);
+
     }
 }
---
+++ src/Node.java
@@ -0,0 +1,11 @@
+
+public class Node {
+    int x;
+    int y;
+
+    public Node(int x, int y) {
+        System.out.println("NodeX:"+x+"NodeY:"+y);
+        this.x = x;
+        this.y = y;
+    }
+}
